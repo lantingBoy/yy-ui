52d77f77bc0fd5d80bb66aaed6e09f6ce78508b0
zhoumengxiang <mengxiang.zhou.h5@gmail.com>
2022-07-25 16:24:44 +0800

    feat: 稳定可打包版本

--- packages/SearchInput/src/index.vue
+++ packages/SearchInput/src/index.vue
@@ -1,135 +1,110 @@
 <template>
-  <div class="input-search-select">
-    <el-input
-      @focus="inputFocus"
-      @blur="inputBlur"
-      v-model.trim="value"
-      v-input-debounce="[inputChange, 300]"
-      :placeholder="placeholder"
-    ></el-input>
-    <div class="select-list" v-show="isShow" :id="id">
-      <div v-loading="loading">
-        <div
-          class="li"
-          v-for="(item) in data.data"
-          :key="item.id"
-          @click="clickItem(item)"
-          :title="item.name"
-        >{{ item.name }}</div>
+  <div class="t-search">
+    <div class="search-main">
+      <div class="search-left">
+        <el-input :placeholder="tips" clearable v-model="currentVal" class="input-with-select">
+          <!-- filterable取消下拉选项搜索功能 -->
+          <el-select
+            v-if="searchCondition.length>0"
+            v-model="currentType"
+            @change="changeSelect"
+            slot="prepend"
+            placeholder="请选择"
+            v-bind="$attrs"
+            v-on="$listeners"
+          >
+            <el-option
+              v-for="(item, index) in searchCondition"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+          <el-button slot="append" icon="el-icon-search" @click="search()">查询</el-button>
+        </el-input>
       </div>
-      <el-button
-        v-if="data.data && data.data.length > 0 && data.data.length < data.total"
-        :loading="btnLoading"
-        class="load-more"
-        @click="getMore"
-      >加载更多({{ data.total }})</el-button>
+      <div class="search-right">
+        <slot name="operate"></slot>
+      </div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'InputSearch',
   data () {
     return {
-      isShow: false,
-      isFocus: false,
-      loading: false,
-      btnLoading: false,
-      value: '',
-      id: new Date().getTime()
+      currentType: this.selectValue,
+      currentVal: this.searchValue
     }
   },
   props: {
-    data: {
-      type: Object,
-      default: () => ({})
-    },
-    placeholder: {
+    searchValue: { // 查询值
       type: String,
       default: ''
-    }
-  },
-  mounted () {
-    window.addEventListener('click', this.onClickOutsideHandler)
-  },
-  // 方法
-  methods: {
-    inputFocus () {
-      this.isShow = true
-      this.isFocus = true
-      this.loading = true
-      this.$emit('inputChange', this.value)
     },
-    inputChange (val) {
-      this.loading = true
-      this.$emit('inputChange', val)
+    selectValue: { // 下拉选项值
+      type: String,
+      default: ''
     },
-    inputBlur () {
-      this.isFocus = false
-    },
-    onClickOutsideHandler (event) {
-      const e = document.getElementById(this.id)
-      if (this.isShow && !e.contains(event.target) && !this.isFocus) {
-        this.isShow = false
+    // eslint-disable-next-line vue/no-dupe-keys
+    searchCondition: { // 下拉选项
+      type: Array,
+      default: () => {
+        return []
       }
     },
-    clickItem (item) {
-      this.isShow = false
-      this.value = item.name
-      this.$emit('clickItem', item)
-    },
-    getMore () {
-      this.btnLoading = true
-      this.$emit('inputChange', this.value, 'more')
+    tips: {
+      type: String,
+      default: '请输入查询内容'
     }
   },
-  destroyed () {
-    window.removeEventListener('click', this.onClickOutsideHandler)
+  methods: {
+    search () {
+      this.$emit('click', this.currentVal)
+    },
+    changeSelect () {
+      // 清空输入框
+      this.currentVal = ''
+      this.$emit('change', this.currentType)
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.input-search-select {
-  position: relative;
-  ::v-deep .el-input-group__append {
-    padding: 0 17px;
+<style lang="scss">
+.t-search {
+  padding: 20px 0;
+  width: 435px;
+  &:before {
+    content: '' !important;
   }
-  .select-list {
-    position: absolute;
-    background-color: #fff;
-    z-index: 1000;
-    width: 100%;
-    border: 1px solid #f1f1f1;
-    box-shadow: 0 0 5px #ccc;
-    border-radius: 4px;
-    max-height: 400px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    > div {
-      min-height: 100px;
+  .search-main {
+    display: flex;
+    justify-content: space-between;
+    .el-input-group__prepend {
+      background-color: #fff;
+      width: 130px;
+      .el-input__inner {
+        text-align: center;
+        width: 130px !important;
+      }
     }
-    .li {
-      border-bottom: 1px solid #f1f1f1;
-      padding: 0 10px;
-      line-height: 40px;
-      cursor: pointer;
-      font-size: 14px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      text-align: left;
+    .search-left {
+      .el-input__inner {
+        width: 260px;
+      }
+    }
+    button {
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
     }
   }
-  ::v-deep .el-loading-spinner .circular {
-    height: 26px;
-    width: 26px;
-  }
-  .load-more {
-    border: 0;
-    width: 100%;
-    border-radius: 0;
+  .search-right {
+    margin-left: 20px;
+    div {
+      display: flex;
+      align-items: center;
+    }
   }
 }
 </style>
